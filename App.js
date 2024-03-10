492585456e8145f2ab3ee958fa02fa8bee63a0bf
Abhishek <abhisheksunkuru@gmail.com>
2024-03-10 19:23:55 +0530

    episode 2

--- App.js
+++ App.js
@@ -1,10 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
 const parent = React.createElement('div',{id: "parent"},[
   React.createElement('div', {id: "child"},[
     React.createElement('h1',{},'i am h1 tag' ),
     React.createElement('h2',{}, 'i am h2 tag')
   ]),
-  React.createElement('div', {id: "child2"},[
-    React.createElement('h1',{},'i am h1 tag' ),
+  React.createElement('div', {id: "child2 adadasds"},[
+    React.createElement('h1',{},'i am h1 tag sddsfsdf' ),
     React.createElement('h2',{}, 'i am h2 tag')
   ])
 ]
