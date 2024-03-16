3cba73d70f6a3d50dc1bf981c6df99ea7c8b8b20
Abhishek <abhisheksunkuru@gmail.com>
2024-03-16 17:16:17 +0530

    episiode4 react

--- App.js
+++ App.js
@@ -1,15 +1,29 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-const parent = React.createElement('div',{id: "parent"},[
-  React.createElement('div', {id: "child"},[
-    React.createElement('h1',{},'i am h1 tag' ),
-    React.createElement('h2',{}, 'i am h2 tag')
-  ]),
-  React.createElement('div', {id: "child2 adadasds"},[
-    React.createElement('h1',{},'i am h1 tag sddsfsdf' ),
-    React.createElement('h2',{}, 'i am h2 tag')
-  ])
-]
+import ReactDOM from 'react-dom/client';
+
+// const heading = React.createElement('h1', 
+// {}, 
+// "Abhishek Sunkuru"
+// )
+const Title = () => (
+  <h1 className='head' tabIndex="5">
+    React Header Component
+  </h1>
+)
+
+// console.log(heading);
+// jsx is transapiled before it reached js engine - parcel -> babel
+// jsx => React.createElement => reactcreateElement js => html element
+// const jsxheading = <h1 id="test">Namaste React ABhi</h1>;
+// console.log(jsxheading);
+const HeadingComponent = () => (
+  <div id="container" >
+    {Title()}
+    <Title/>
+    <h1 className="heading"> Namaste React Functional Component</h1>
+  </div>
 )
 const root = ReactDOM.createRoot(document.getElementById('root'))
-root.render(parent)
+
+
+root.render(<HeadingComponent />)
---
+++ README.md
@@ -0,0 +1,19 @@
+# Namaste React
+
+# Parcel
+- Dev build
+- local server
+- HMR- hot module replacement
+- file diff algorithm
+- image optimization
+- caching
+- minifaction
+- compression
+- bundling
+- consistent hashing
+- code splitting
+- differential bundling
+- error handing
+- https
+- tree shaking
+- dev build and production
